f91a9df9144aa62c89cc38805a72033650572cab
sucre <sucre@youmi.com>
2019-01-04 00:05:13 +0800

    js

---
+++ src/com/sucre/js/test.java
@@ -0,0 +1,13 @@
+package com.sucre.js;
+
+import javax.swing.*;
+
+public class test {
+
+    private JButton runButton;
+    private JTextArea textArea1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+}
+}
