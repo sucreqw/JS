07aa6b3c230a366f8f8d3a725b0295792e27f71e
sucre <sucre@youmi.com>
2019-01-04 14:24:50 +0800

    JS
    add java code formart.

--- src/com/sucre/js/test.java
+++ src/com/sucre/js/test.java
@@ -1,13 +1,47 @@
 package com.sucre.js;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class test {
-
+    private JPanel jpanle;
     private JButton runButton;
-    private JTextArea textArea1;
+    private JTextArea source;
+    private JComboBox mission;
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
-}
+    public test() {
+        runButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String m=(String)mission.getSelectedItem();
+                switch (m){
+
+                    case "java" :
+                        String sourceText=source.getText();
+                        String temp="private byte[] getdata(String cookies, String uid) {\r\n"
+                                  + "    StringBuilder data = new StringBuilder(900);\r\n"
+                                  + "    String temp = \"\";\r\n"
+                                  + "    data.append(\""+sourceText.replace("\n","\");\r\n    data.append(")//")\r\ndata.append("
+
+                                  + "\")\r\nreturn data.toString().getBytes();\r\n"
+                                  + "}\r\n"
+
+                        ;
+                        source.setText(temp);
+                        break;
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("test");
+        frame.setContentPane(new test().jpanle);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(200, 100, 807, 499);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
 }
